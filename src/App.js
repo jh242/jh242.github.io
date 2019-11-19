ed368cbcc3f7cf9c5bfc03fff4131a52e21d4aa7
Jack Hu <jackhu242@gmail.com>
2019-11-18 23:07:02 -0500

    added formatting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
-import { Grommet, Box, Text, Image } from 'grommet';
+import React, { Component } from 'react'
+import { Grommet, Box, Text, Image } from 'grommet'
 import { back } from './assets/back.png'
 
 class App extends Component {
-
-  render() {
-
+  render () {
     const theme = {
 
     }
 
     return (
       <Grommet theme={theme}>
-        <Box height="100vh" align="center" justify="center">
+        <Box height='100vh' align='center' justify='center'>
           <Text>This site is under construction. Check back soon!</Text>
         </Box>
       </Grommet>
-    );
+    )
   }
 }
 
-export default App;
+export default App
