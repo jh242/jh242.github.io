76bbbca5eee44d28c9e83bf2b03f98c7f815fb84
Jack Hu <jackhu242@gmail.com>
2019-11-18 20:11:12 -0500

    Added some cool stuff

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import 'semantic-ui-css/semantic.min.css';
-import './App.css';
+import React, { Component } from 'react';
+import { Grommet, Box, Text } from 'grommet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+
+    const theme = {
+
+    }
+
+    return (
+      <Grommet theme={theme}>
+        <Box align="center" justify="center" height="100vh">
+          <Text>This page isn't ready yet! Check back soon.</Text>
+        </Box>
+      </Grommet>
+    );
+  }
 }
 
 export default App;
