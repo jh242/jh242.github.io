5f12139a3f5389ae5072597a785d7aa92910212b
Jack Hu <jackhu242@gmail.com>
2020-01-24 10:31:27 -0500

    Updates

--- precache-manifest.1e38c47912adfe05be6c39515ac2ba53.js
+++ precache-manifest.1e38c47912adfe05be6c39515ac2ba53.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2a24d6aa00d1171404e6dee10c1adc6b",
+    "revision": "17473375a95ed2f8a0027a0ff9620237",
     "url": "/jh242.github.io/index.html"
   },
   {
-    "revision": "3763d774726d9e249a5b",
+    "revision": "c7c7f2cb2ca26fd0f128",
     "url": "/jh242.github.io/static/css/main.498d002a.chunk.css"
   },
   {
@@ -12,15 +12,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/jh242.github.io/static/js/2.f39e8cc6.chunk.js"
   },
   {
-    "revision": "3763d774726d9e249a5b",
-    "url": "/jh242.github.io/static/js/main.313ce467.chunk.js"
+    "revision": "c7c7f2cb2ca26fd0f128",
+    "url": "/jh242.github.io/static/js/main.1f2dd331.chunk.js"
   },
   {
     "revision": "efd7ca28457aca6caeb9",
     "url": "/jh242.github.io/static/js/runtime-main.4f7f38b0.js"
   },
   {
-    "revision": "185685475509d996c85106dc7229908f",
-    "url": "/jh242.github.io/static/media/JackHuResume.18568547.pdf"
+    "revision": "145a826953e7073d8fcf1f14f347545f",
+    "url": "/jh242.github.io/static/media/JackHuResume.145a8269.pdf"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jh242.github.io/precache-manifest.96dac98e16abdb4c9553a1bb24e894fb.js"
+  "/jh242.github.io/precache-manifest.1e38c47912adfe05be6c39515ac2ba53.js"
 );
 
 self.addEventListener('message', (event) => {
