7ca8f170de4c854ed303731b630664fb10bb0bb3
Jack Hu <jackhu242@gmail.com>
2020-01-31 23:08:09 -0500

    Added basic content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
+import { motion } from 'framer-motion';
+import './App.css';
 import resume from './assets/JackHuResume.pdf';
 
 class App extends Component {
 
-  constructor(props) {
+  /*constructor(props) {
     super(props);
 
     this.state = {};
-  }
+  }*/
 
   render() {
 
+    return (
+      <div className="App">
+        <div className="Intro">
+          <motion.div className="Terminal" initial={{ opacity: 0 }} animate={{ opacity: 1, width: 960, height: 540 }} transition={{ ease: "easeIn", duration: 1.5 }}>
+            <motion.h1>Jack Hu</motion.h1>
+            <motion.p>Professional. Fun-loving. All at the same time.</motion.p>
+          </motion.div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import './assets/fonts/SFProDisplay.ttf';
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
