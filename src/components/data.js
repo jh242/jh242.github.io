85bbab42107449bbf3cc3904aac2ad213aae5b6d
Jack Hu <jackhu242@gmail.com>
2020-07-23 17:29:20 -0600

    Added flexmirror

--- src/components/data.js
+++ src/components/data.js
@@ -10,6 +10,11 @@ const items = [
     details: "Many hospitals across the world still use paper for keeping records. Healthdex provides a seamless way to transition to digital using Google Cloud's DocumentAI. It can scan any healthcare records and convert them, making it easy to search, access, and store patient info long term. This project was created for TOHacks 2020.",
   },
   {
+    id: 'flexmirror',
+    title: 'Flex Smart Mirror',
+    details: 'The first project I worked on in University, the Flex Mirror was a smart mirror powered by a Raspberry Pi running a React app wrapped in Electron for the UI. It used the IBM Watson API for voice assistant, and Python OpenCV for face unlock capabilities.',
+  },
+  {
     id: 'schola',
     title: 'Schola',
     details: 'This was a project developed for YHack 2019. Schola combines Python-based OpenCV for motion tracking with an Arduino in order to track a professor moving around a lecture hall. Then, this video feed would be streamed with Flask to a web portal with Firebase authentication.',
